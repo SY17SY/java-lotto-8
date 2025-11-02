74eb05b11cbcec7a7d2c5ce739e769a505170cf1
yeong <rlatjdud6573@naver.com>
2025-11-02 19:47:46 +0900

    feat: LottoFactory, LottoAutoFactory 구현

---
+++ src/main/java/lotto/domain/LottoAutoFactory.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoAutoFactory implements LottoFactory {
+    @Override
+    public Lotto generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+}
