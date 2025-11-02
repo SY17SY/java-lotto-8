ca3b5f6a035025a81ab02e77966ed164460df5ef
yeong <rlatjdud6573@naver.com>
2025-11-03 00:45:25 +0900

    fix: Rank from matches 생성자 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,14 +1,25 @@
 package lotto.domain;
 
+import java.util.Map;
+
 public enum Rank {
     FIRST("6개 일치", 2000000000),
     SECOND("5개 일치, 보너스 볼 일치", 30000000),
     THIRD("5개 일치", 1500000),
     FOURTH("4개 일치", 50000),
-    FIFTH("3개 일치", 5000);
+    FIFTH("3개 일치", 5000),
+    MISS("", 0);
 
-    private static final String PREFIX = "(";
-    private static final String SUFFIX = ") - ";
+    private static final Map<Integer, Rank> NON_BONUS_MAP = Map.of(
+            6, FIRST,
+            4, FOURTH,
+            3, FIFTH
+    );
+
+    private static final Map<Boolean, Rank> BONUS_MAP = Map.of(
+            true, SECOND,
+            false, THIRD
+    );
 
     private final String promptHead;
     private final int prizeMoney;
@@ -18,8 +29,15 @@ public enum Rank {
         this.prizeMoney = prizeMoney;
     }
 
+    public static Rank fromMatches(int matches, boolean bonus) {
+        if (matches == 5) {
+            return BONUS_MAP.get(bonus);
+        }
+        return NON_BONUS_MAP.getOrDefault(matches, MISS);
+    }
+
     public String getPrompt() {
-        return promptHead + PREFIX + prizeMoney + SUFFIX;
+        return promptHead + String.format(" (%,d원) -", prizeMoney);
     }
 
     public int getPrizeMoney() {
