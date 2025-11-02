01d5e288c407e29f92698b019341978c918ede66
yeong <rlatjdud6573@naver.com>
2025-11-02 20:57:04 +0900

    feat: LottoController 수정, Exception 발생 시 재시도

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,8 @@
 package lotto.controller;
 
+import lotto.domain.LottoAutoFactory;
+import lotto.dto.LottosDto;
+import lotto.service.LottoService;
 import lotto.view.ErrorView;
 import lotto.view.InputView;
 import lotto.view.PromptView;
@@ -8,19 +11,31 @@ public class LottoController {
     private final PromptView promptView;
     private final InputView inputView;
     private final ErrorView errorView;
+    private final LottoService lottoService;
 
-    public LottoController(PromptView promptView, InputView inputView, ErrorView errorView) {
+    public LottoController(PromptView promptView, InputView inputView, ErrorView errorView, LottoService lottoService) {
         this.promptView = promptView;
         this.inputView = inputView;
         this.errorView = errorView;
+        this.lottoService = lottoService;
     }
 
     public void run() {
         Parser parser = new Parser(errorView);
 
-        promptView.printPromptPayment();
-        String inputPayment = inputView.inputPayment();
-        int payment = parser.parsePayment(inputPayment);
+        boolean success = false;
+
+        while (!success) {
+            try {
+                promptView.printPromptPayment();
+                String inputPayment = inputView.inputPayment();
+                int payment = parser.parsePayment(inputPayment);
+                LottosDto lottosDto = lottoService.generate(payment, new LottoAutoFactory());
+                success = true;
+            } catch (IllegalArgumentException e) {
+                errorView.printError(e);
+            }
+        }
     }
 }
 
@@ -32,10 +47,6 @@ class Parser {
     }
 
     int parsePayment(String inputPayment) {
-        try {
-            return Integer.parseInt(inputPayment);
-        } catch (NumberFormatException e) {
-            errorView.printPaymentSyntaxError();
-        }
+        return Integer.parseInt(inputPayment);
     }
 }
