53e5bfe6c45d0951458c344cd1fa9614687f7095
yeong <rlatjdud6573@naver.com>
2025-11-02 20:26:39 +0900

    feat: LottoAutoFactory 오름차순으로 수정

--- src/main/java/lotto/domain/LottoAutoFactory.java
+++ src/main/java/lotto/domain/LottoAutoFactory.java
@@ -2,11 +2,15 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoAutoFactory implements LottoFactory {
     @Override
     public Lotto generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return new Lotto(numbers);
+        List<Integer> sorted = numbers.stream()
+                .sorted()
+                .toList();
+        return new Lotto(sorted);
     }
 }
