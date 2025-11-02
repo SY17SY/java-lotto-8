843371345cac9a8d51cba445b4468c3bdedcbe66
yeong <rlatjdud6573@naver.com>
2025-11-02 21:00:10 +0900

    feat: InputView 통합화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,7 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String inputPayment() {
+    public String inputLine() {
         return Console.readLine();
     }
 }
