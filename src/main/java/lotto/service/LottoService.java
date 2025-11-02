086e086713e6e9099e623eec9728838eb5ef8d57
yeong <rlatjdud6573@naver.com>
2025-11-03 00:56:32 +0900

    feat: LottoService calculate 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,13 +1,22 @@
 package lotto.service;
 
+import java.util.List;
 import lotto.domain.LottoFactory;
 import lotto.domain.Lottos;
+import lotto.domain.Result;
 import lotto.dto.LottosDto;
+import lotto.dto.ResultDto;
 import lotto.service.mapper.LottosMapper;
+import lotto.service.mapper.ResultMapper;
 
 public class LottoService {
     public LottosDto generate(int payment, LottoFactory factory) {
         Lottos lottos = Lottos.from(payment, factory);
         return LottosMapper.toDto(lottos);
+    }
+
+    public ResultDto calculate(LottosDto lottosDto, List<Integer> winNumbers, int bonusNumber) {
+        Result result = Result.from(lottosDto, winNumbers, bonusNumber);
+        return ResultMapper.toDto(result);
     }
 }
