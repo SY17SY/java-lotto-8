c8bc226c134c75eaea5cc9c22c8d59b39cf2b550
yeong <rlatjdud6573@naver.com>
2025-11-03 01:45:47 +0900

    feat: Application 의존성 주입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.view.ErrorView;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import lotto.view.PromptView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PromptView promptView = new PromptView();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        ErrorView errorView = new ErrorView();
+        LottoService lottoService = new LottoService();
+
+        LottoController controller = new LottoController(
+                promptView,
+                inputView,
+                outputView,
+                errorView,
+                lottoService
+        );
+
+        controller.run();
     }
 }
