2adcbad456afb89b8c6bbe8bfeead462154a272f
yeong <rlatjdud6573@naver.com>
2025-11-02 21:57:25 +0900

    feat: ErrorMessage 추가

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 public enum ErrorMessage {
+    PAYMENT_BLANK("구입 금액을 작성해 주십시오."),
     PAYMENT_SYNTAX("구입 금액의 형식이 올바르지 않습니다."),
     PAYMENT_NEGATIVE("구입 금액은 0보다 커야 합니다."),
     PAYMENT_REMAINDER("구입 금액은 1,000원 단위이어야 합니다."),
@@ -8,7 +9,9 @@ public enum ErrorMessage {
     LOTTO_NUMBERS_DUPLICATION("로또 번호는 서로 다른 수이어야 합니다."),
     WIN_NUMBER_BLANK("당첨 번호를 작성해 주십시오."),
     WIN_NUMBER_DELIMITER("당첨 번호가 쉼표(,)로 구분되지 않았습니다."),
-    WIN_NUMBER_SYNTAX("당첨 번호의 형식이 올바르지 않습니다.");
+    WIN_NUMBER_SYNTAX("당첨 번호의 형식이 올바르지 않습니다."),
+    BONUS_NUMBER_BLANK("보너스 번호를 작성해 주십시오."),
+    BONUS_NUMBER_SYNTAX("보너스 번호의 형식이 올바르지 않습니다.");
 
     private static final String ERROR = "[ERROR] ";
 
