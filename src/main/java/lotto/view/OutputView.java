52f4a53c404333f4ad097d5ba525e2e1e443ea42
yeong <rlatjdud6573@naver.com>
2025-11-03 01:31:49 +0900

    feat: 수익률 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,7 +7,7 @@ import lotto.dto.ResultDto;
 
 public class OutputView {
     public void printLottos(LottosDto lottosDto) {
-        System.out.println(lottosDto.count() + "개를 구매했습니다.");
+        System.out.println(lottosDto.count() + OutputMessage.LOTTOS.getMessage());
         for (LottoDto lottoDto : lottosDto.lottos()) {
             System.out.println(lottoDto.numbers());
         }
@@ -19,5 +19,9 @@ public class OutputView {
             int count = resultDto.rankCounts().get(rank);
             System.out.println(rank.getPrompt() + String.format("%d개", count));
         }
+    }
+
+    public void printProfitRate(double profitRate) {
+        System.out.println(OutputMessage.PROFIT_RATE_PREFIX.getMessage() + String.format("%.1f", profitRate) + OutputMessage.PROFIT_RATE_SUFFIX);
     }
 }
