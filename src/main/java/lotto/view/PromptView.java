efeebd11f581e220993e28b717fa36c0411d0c0e
yeong <rlatjdud6573@naver.com>
2025-11-02 21:58:08 +0900

    feat: PromptView - BonusNumber 입력 추가

--- src/main/java/lotto/view/PromptView.java
+++ src/main/java/lotto/view/PromptView.java
@@ -8,4 +8,8 @@ public class PromptView {
     public void printPromptWinNumber() {
         System.out.println(PromptMessage.WIN_NUMBER.getMessage());
     }
+
+    public void printPromptBonusNumber() {
+        System.out.println(PromptMessage.BONUS_NUMBER.getMessage());
+    }
 }
