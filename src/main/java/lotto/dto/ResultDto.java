18be7cbbd7dfafcc481e2922b53102f4c98e1237
yeong <rlatjdud6573@naver.com>
2025-11-03 01:31:05 +0900

    feat: ResultDto - profit 추가

--- src/main/java/lotto/dto/ResultDto.java
+++ src/main/java/lotto/dto/ResultDto.java
@@ -3,5 +3,5 @@ package lotto.dto;
 import java.util.Map;
 import lotto.domain.Rank;
 
-public record ResultDto(Map<Rank, Integer> rankCounts) {
+public record ResultDto(Map<Rank, Integer> rankCounts, int profit) {
 }
