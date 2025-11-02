c08492ab7fcdef4326e6646ad2744876a0a3a77d
yeong <rlatjdud6573@naver.com>
2025-11-03 03:08:23 +0900

    refactor: Rank 재정렬

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -5,8 +5,8 @@ import java.util.Map;
 public enum Rank {
     MISS("", 0),
     FIFTH("3개 일치", 5000),
-    THIRD("5개 일치", 1500000),
     FOURTH("4개 일치", 50000),
+    THIRD("5개 일치", 1500000),
     SECOND("5개 일치, 보너스 볼 일치", 30000000),
     FIRST("6개 일치", 2000000000);
 
