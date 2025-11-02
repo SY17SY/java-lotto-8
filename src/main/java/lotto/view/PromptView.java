b1b962dd1578474ed9237a80195b260956a28a2d
yeong <rlatjdud6573@naver.com>
2025-11-02 20:59:03 +0900

    feat: printPromptWinNumber 구현

--- src/main/java/lotto/view/PromptView.java
+++ src/main/java/lotto/view/PromptView.java
@@ -4,4 +4,8 @@ public class PromptView {
     public void printPromptPayment() {
         System.out.println(PromptMessage.PAYMENT.getMessage());
     }
+
+    public void printPromptWinNumber() {
+        System.out.println(PromptMessage.WIN_NUMBER.getMessage());
+    }
 }
