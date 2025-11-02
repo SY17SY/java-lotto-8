f521aaffb52641a0ab0799cf95453645bb508b85
yeong <rlatjdud6573@naver.com>
2025-11-02 20:43:43 +0900

    feat: Lotto validate 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import java.util.List;
 import lotto.dto.LottoDto;
+import lotto.view.ErrorMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,7 +18,10 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_LENGTH.getMessage());
+        }
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_DUPLICATION.getMessage());
         }
     }
 }
