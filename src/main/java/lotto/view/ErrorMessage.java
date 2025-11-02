d9d42a7fd9fa9ff0fa892d668083210955d6df42
yeong <rlatjdud6573@naver.com>
2025-11-02 20:43:25 +0900

    feat: ErrorMessage: 로또 번호 비즈니스 검증 메시지 추가

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -3,7 +3,9 @@ package lotto.view;
 public enum ErrorMessage {
     PAYMENT_SYNTAX("구입 금액의 형식이 올바르지 않습니다."),
     PAYMENT_NEGATIVE("구입 금액은 0보다 커야 합니다."),
-    PAYMENT_REMAINDER("구입 금액은 1,000원 단위이어야 합니다.");
+    PAYMENT_REMAINDER("구입 금액은 1,000원 단위이어야 합니다."),
+    LOTTO_NUMBERS_LENGTH("로또 번호는 6개여야 합니다."),
+    LOTTO_NUMBERS_DUPLICATION("로또 번호는 서로 다른 수이어야 합니다.");
 
     private static final String ERROR = "[ERROR] ";
 
