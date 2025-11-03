40b992f90589122200639e04b623ddf294934eea
yeong <rlatjdud6573@naver.com>
2025-11-03 14:51:25 +0900

    refactor: ErrorView 개행 추가

--- src/main/java/lotto/view/ErrorView.java
+++ src/main/java/lotto/view/ErrorView.java
@@ -3,5 +3,6 @@ package lotto.view;
 public class ErrorView {
     public void printError(IllegalArgumentException e) {
         System.out.println(e.getMessage());
+        System.out.println();
     }
 }
